62d79ce99016093ab0bd2c3750db60209a98fde1
iamsandeepkumarmandal <sandeep.sandeep.mandal@gmail.com>
2017-11-16 08:07:40 +0530

    Script changes

--- fracktal/script/scripts.js
+++ fracktal/script/scripts.js
@@ -1,3 +1,15 @@
+$(document).keydown(function(event){
+    if(event.keyCode==123){
+        return false;
+    }
+    else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+             return false;
+    }
+});
+
+$(document).on("contextmenu",function(e){        
+   e.preventDefault();
+});
 
 
 jQuery(document).ready(function () {
