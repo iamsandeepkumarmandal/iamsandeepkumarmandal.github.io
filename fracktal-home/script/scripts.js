1f23cbd82ad4adb18b31e0abbb9bbc8c69d16b74
Sandeep <sandeep.sandeep.mandal@gmail.com>
2018-04-06 15:08:14 +0530

    Changes by rohit

--- fracktal-home/script/scripts.js
+++ fracktal-home/script/scripts.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function () {
     var $root = $('html, body');
     $('.smoothScroll').click(function () {
         $root.animate({
-            scrollTop: $($.attr(this, 'data-target')).offset().top - 64
+            scrollTop: $($.attr(this, 'data-target')).offset().top - 180
         }, 500);
         return false;
     });
