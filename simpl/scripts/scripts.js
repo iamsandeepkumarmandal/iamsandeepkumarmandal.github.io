ca430123f869d3fb74ccf6cece2602b6b7291b2d
Sandeep Kumar Mandal <sandeep.sandeep.mandal@gmail.com>
2018-11-23 17:27:16 +0530

    Simple Dashboard changes

--- simpl/scripts/scripts.js
+++ simpl/scripts/scripts.js
@@ -14,3 +14,21 @@ if(deviceWidth < 768) {
         $(this).addClass('active').next().slideDown(400);
     });
 }
+$('.show-transaction-details_js').click(function(){
+    let getId = $(this).attr('data-target-body');
+    $(this).toggleClass('active');
+    $(getId).slideToggle(400);
+});
+
+$(".customer-tabs__links-wrapper li a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
