ea9e697a8b20e2bc52923324b125b3da63d183d4
Sandeep Kumar Mandal <sandeep@videoken.com>
2019-12-30 13:33:56 +0530

    Chnages

--- videolake-dashboard/scripts/scripts.js
+++ videolake-dashboard/scripts/scripts.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
     $('.close-search-slide-popup_js').click(function() {
         $('.search-slide-popup_js').removeClass('active');  
     });
+    $.ajax({
+        url: 'https://stage-edit.videoken.com/flask_restful/todo/api/v1.0/tasks',
+        method: 'get',
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(error) {
+          console.log(error);
+        },
+    })
 });
